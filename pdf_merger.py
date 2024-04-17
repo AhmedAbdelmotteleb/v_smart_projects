9d4f12fd73332bc4e66b8344949e340112dbc2e4
Ahmed Abdelmotteleb <ahmedsamehwagih@gmail.com>
2024-04-17 17:37:14 +0100

    Improve pdf_merger

--- pdf_merger.py
+++ pdf_merger.py
@@ -4,46 +4,53 @@ Last updated: 15 Apr 2024
 Script that merges PDF files in a directory.
 """
 
-import PyPDF2
+from PyPDF2 import PdfMerger
 import argparse
 import os
+import sys
 
 def main():
     parser = argparse.ArgumentParser(description='Merge PDF files in a directory')
 
-    parser.add_argument('--dir', required=True, type=str, help='the directory to merge PDFs from')
+    parser.add_argument('--files-dir', default='.', type=str, help='the directory to merge PDFs from')
+    parser.add_argument('--output-dir', default='.', type=str, help='the directory to save the merged PDF')
     parser.add_argument('--output', required=True, type=str, help='the output file name')
     parser.add_argument('--sort', action='store_true', help='sort the files before merging')
     parser.add_argument('--files', nargs='*', help='specific files to merge')
 
     args = parser.parse_args()
 
-    if not os.path.exists(args.dir):
-        print(f'Directory {args.dir} does not exist.')
-        exit(1)
+    if not args.output.endswith('.pdf'):
+        raise ValueError('Output file must be a PDF file. Use the .pdf extension.')
 
-    if os.path.exists(args.output):
-        should_overwrite = input(f"File {args.output} already exists. Do you want to overwrite it? (y/n): ")
+    if not os.path.exists(args.files_dir):
+        raise FileNotFoundError(f'Directory {args.files_dir} does not exist.')
+    
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    output_file_path = os.path.join(args.output_dir, args.output)
+    if os.path.exists(output_file_path):
+        should_overwrite = input(f"File {output_file_path} already exists. Do you want to overwrite it? (y/n): ")
         if should_overwrite.lower() != 'y':
             print("Exiting without overwriting the file.")
-            exit(0)
+            sys.exit(0)
 
-    merger = PyPDF2.PdfFileMerger()
+    merger = PdfMerger()
 
-    files = args.files if args.files else os.listdir(args.dir)
+    files = args.files if args.files else os.listdir(args.files_dir)
     if args.sort:
         files.sort()
 
     pdf_files = [file for file in files if file.endswith('.pdf')]
 
     if not pdf_files:
-        print(f"No PDF files found in directory {args.dir}.")
-        exit(1)
+        raise FileNotFoundError(f"No PDF files found in directory {args.files_dir}.")
 
     for file in pdf_files:
-        merger.append(os.path.join(args.dir, file))
-
-    merger.write(args.output)
+        merger.append(os.path.join(args.files_dir, file))
+        
+    print('Merging PDF files...')
+    merger.write(output_file_path)
     merger.close()
 
 if __name__ == "__main__":
